4fb3d9ed4edfc4811914bfb9b9d58d83e4868da5
339713273 <339713273@2260I-L29-026>
2024-12-12 09:19:13 -0500

    Add project files.

---
+++ Summative Animation Assignment/Game1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Summative_Animation_Assignment
+{
+    enum Screen
+    {
+        Intro,
+        RaceTrack, 
+        end
+    }
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        SpriteFont textFont;
+
+        Texture2D  introTexture;
+
+        Screen screen;
+
+        MouseState mouseState;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+
+            screen = Screen.Intro;
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            introTexture = Content.Load<Texture2D>("track");
+            textFont = Content.Load<SpriteFont>("File");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            mouseState = Mouse.GetState();
+
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed) 
+                screen = Screen.RaceTrack;
+            }
+
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introTexture, window, Color.White);
+                _spriteBatch.DrawString(textFont, "Left Click to Continue to the Game!", new Vector2(150,10), Color.Black);
+            }
+            
+
+
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
